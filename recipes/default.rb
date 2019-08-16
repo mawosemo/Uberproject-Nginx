9c3eb6bef335ca12c06246f97818a7b4600645ff
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-08-16 11:44:37 +0100

    created a template proxy.conf file with a proxy pass added, added code needed to now complete the unit tests

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+apt_update 'update_sources' do
+    action :update
+end
+
+
+package "nginx"
+# package "nodejs"
+service "nginx" do
+  action :enable
+end
+service "nginx" do
+  action :start
+end
