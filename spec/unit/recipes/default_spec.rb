e03f96509d2136ced3b43d387de12aff5325d9af
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-08-16 11:38:38 +0100

    added unitest to install, enable and start nginx - they failed as expected

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -17,13 +17,13 @@ describe 'Nginx::default' do
     end
   end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+  it 'should install nginx'do
+    expect(chef_run).to install_package "nginx"
+  end
+  it 'should enable nginx'do
+    expect(chef_run).to enable_service "nginx"
+  end
+  it 'should be start nginx'do
+    expect(chef_run).to start_service "nginx"
   end
 end
